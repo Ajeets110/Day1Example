0098d525542998d1bdf211b71a6f6e367455ee69
Ajeet <ajeets110@gmail.com>
2019-10-07 20:39:09 -0400

    Day1

--- Day1Example/main.swift
+++ Day1Example/main.swift
@@ -7,6 +7,65 @@
 //
 
 import Foundation
-
 print("Hello!")
+
+
+/*
+ var str = "Hello, playground"
+ //str = 100
+ print(str)
+ var s: Int = 100
+ print(s)
+ 
+ 
+ 
+ //a = 300
+ 
+ print(a)
+ 
+ 
+ // A d d
+ let a = 200; var b = 200
+ var c = a + b
+ var sum = "SUM of \(a) and \(b) is \(c)"
+ print(sum)
+ 
+ //readLine()
+ 
+ var p = "🗽"
+ print(p)
+ 
+ var 🦁 = "Lion"
+ 
+ print(🦁)
+ 
+ var x1 = (1, "Ajeet" , "Singh")
+ print(x1)
+ print(x1.0)
+ print(x1.1)
+ print(x1.2)
+ */
+var x2 = (collegeID : 10, collegeName : "Lambton College", cityName: "Toronto")
+/*print(x2)
+ print(x2.collegeID)
+ print(x2.collegeName)
+ print(x2.cityName)
+ print(x2.self)
+ */
+/*
+ var x3 = (x2.collegeName, x2.cityName)
+ print(x3)
+ */
+/*
+ var _ = 10000
+ var (_, collegName, cityName) = x2
+ 
+ print(x2)
+ */
+for r in 0...1 {
+    print(r)
+}
+for i in stride(from: 10, to: 1, by: -1) {
+    print("Ajeet \(i)")
+}
 
